90de1554f8a951571cde826b3380c2f728354b14
AliMoosa0 <alm9ly@gmail.com>
2023-12-11 17:30:04 +0300

    fixed the search

--- home.php
+++ home.php
@@ -18,7 +18,7 @@
     ?>
     <div>
 
-       
+
         <button onclick="filterBooks('ICT')">ICT</button>
         <button onclick="filterBooks('Web Media')">Web Media</button>
         <button onclick="filterBooks('Engineering')">Engineering</button>
@@ -32,63 +32,96 @@
     <div id="bookDisplay">
         <!-- Book display area -->
         <!-- Display books based on the selected category using AJAX -->
-        <!-- TODO: Display ALL books  -->
+        <!-- display all books  -->
         <?php
-         // Code for displaying multiple books
-    $books = new Books();
-    $row = $books->getBooks();
-    echo "<h1>Welcome to Our Second-Hand Book Store</h1>";
-    echo '
+        if (isset($_GET['keyword'])) {
+            $keyword = $_GET['keyword'];
+            $books = $book->initWithIdOrName($keyword);
+            if ($books) {
+                foreach ($books as $bookInfo) {
+                    // Your code to display book information for each book in $books
+                    $bookId = $bookInfo->bookID;
+                    $bookName = $bookInfo->bookName;
+                    $bookPrice = $bookInfo->bookPrice;
+                    $publishDate = $bookInfo->publishDate;
+                    $bookPic = $bookInfo->bookPic;
+
+                    echo "<div class='book'>";
+                    echo "<img src='uploads/$bookPic' alt='$bookPic'>";
+                    echo "<div class='book-info'>";
+                    echo "<h2>" . $bookName . "</h2>";
+                    echo "<p>Price: " . $bookPrice . "</p>";
+                    echo "<p>Publish Date: " . $publishDate . "</p>";
+                    echo "<button>Add to Cart</button>";
+
+                    echo "</div>";
+                    echo "</div>";
+
+                    echo "</div>";
+                }
+            } else {
+                echo "No books found.";
+            }
+        } else {
+            // Code for displaying multiple books
+            $books = new Books();
+            $row = $books->getBooks();
+            echo "<h1>Welcome to Our Second-Hand Book Store</h1>";
+            echo '
         <form action="" method="GET">
         <label for="keyword">Search by Book ID or Name:</label>
         <input type="text" id="keyword" name="keyword" placeholder="Enter Book ID or Name">
         <button type="submit">Search</button>
          </form>';
 
-    if (isset($_SESSION['username'])) {
-        // Displaying user-specific content if logged in
-        echo "<h1>Welcome, " . $_SESSION['username'] . "</h1>";
-        echo '<a href="addBooks.php"><button>Add a Book</button></a>';
-    }
-
-    // Displaying multiple books with an 'Add to Cart' button for each
-    echo "<div class='books'>";
-    for ($i = 0; $i < count($row); $i++) {
-        echo "<div class='book'>";
-        echo "<img src='uploads/" . $row[$i]->bookPic . "' />";
-        echo "<div class='book-info'>";
-        echo "<h2>" . $row[$i]->bookName . "</h2>";
-        echo "<p>Price: " . $row[$i]->bookPrice . "</p>";
-        echo "<p>Publish Date: " . $row[$i]->publishDate . "</p>";
-        echo "<a href='viewBookDetails.php?bookId=" . $row[$i]->bookID . "'><button>View details</button></a>";
-        echo "<br>";
-        echo '<a href="edit_books.php?id=' . $row[$i]->bookID . '"><button>edit Book</button></a>';
-        echo "<br>";
-        echo '<a href="delete_books.php?id=' . $row[$i]->bookID . '"><button>Delete Book</button></a>';
-
-        // Form for adding a book to the cart
-        echo "<form method='post'>";
-        echo "<input type='hidden' name='bookID' value='" . $row[$i]->bookID . "'>";
-        echo "<input type='submit' name='btnCart' value='Add to Cart'>";
-        echo "</form>";
-
-        // Check if 'Add to Cart' button for a specific book is clicked
-        if (isset($_POST['btnCart']) && isset($_POST['bookID']) && $_POST['bookID'] == $row[$i]->bookID) {
-            $cart = new Cart();
-            $cart->initWith($_SESSION['uid'], $row[$i]->bookID, $row[$i]->bookName, $row[$i]->bookPrice, $row[$i]->bookPic, "show");
-            if ($cart->addToCart($_SESSION['uid'])) { // Pass the necessary argument (in this case, user ID)
-                echo "Book added to cart";
-            } else {
-                echo "Failed to add book to cart";
+            if (isset($_SESSION['username'])) {
+                // Displaying user-specific content if logged in
+                echo "<h1>Welcome, " . $_SESSION['username'] . "</h1>";
+                echo '<a href="addBooks.php"><button>Add a Book</button></a>';
             }
+
+            // Displaying multiple books with an 'Add to Cart' button for each
+            echo "<div class='books'>";
+            for ($i = 0; $i < count($row); $i++) {
+                echo "<div class='book'>";
+                echo "<img src='uploads/" . $row[$i]->bookPic . "' />";
+                echo "<div class='book-info'>";
+                echo "<h2>" . $row[$i]->bookName . "</h2>";
+                echo "<p>Price: " . $row[$i]->bookPrice . "</p>";
+                echo "<p>Publish Date: " . $row[$i]->publishDate . "</p>";
+                echo "<a href='viewBookDetails.php?bookId=" . $row[$i]->bookID . "'><button>View details</button></a>";
+                echo "<br>";
+                echo '<a href="edit_books.php?id=' . $row[$i]->bookID . '"><button>edit Book</button></a>';
+                echo "<br>";
+                echo '<a href="delete_books.php?id=' . $row[$i]->bookID . '"><button>Delete Book</button></a>';
+
+                // Form for adding a book to the cart
+                echo "<form method='post'>";
+                echo "<input type='hidden' name='bookID' value='" . $row[$i]->bookID . "'>";
+                echo "<input type='submit' name='btnCart' value='Add to Cart'>";
+                echo "</form>";
+
+                // Check if 'Add to Cart' button for a specific book is clicked
+                if (isset($_POST['btnCart']) && isset($_POST['bookID']) && $_POST['bookID'] == $row[$i]->bookID) {
+                    $cart = new Cart();
+                    $cart->initWith($_SESSION['uid'], $row[$i]->bookID, $row[$i]->bookName, $row[$i]->bookPrice, $row[$i]->bookPic, "show");
+                    if ($cart->addToCart($_SESSION['uid'])) { // Pass the necessary argument (in this case, user ID)
+                        echo "Book added to cart";
+                    } else {
+                        echo "Failed to add book to cart";
+                    }
+                }
+
+                echo "</div>"; // Closing div for 'book-info'
+                echo "</div>"; // Closing div for 'book'
+            }
+            echo "</div>"; // Closing div for 'books'
         }
 
-        echo "</div>"; // Closing div for 'book-info'
-        echo "</div>"; // Closing div for 'book'
-    }
-    echo "</div>"; // Closing div for 'books'
-    ?>
-        
+        include "footer.php";   
+
+            ?>
+
     </div>
 
     <script>
@@ -101,7 +134,7 @@
                     document.getElementById("bookDisplay").innerHTML = this.responseText;
                 }
             };
-            xhttp.open("GET", "category.php?category=" + category, true); 
+            xhttp.open("GET", "category.php?category=" + category, true);
             xhttp.send();
         }
     </script>
@@ -112,36 +145,6 @@
 
     }
 
-    if (isset($_GET['keyword'])) {
-        $keyword = $_GET['keyword'];
-        $books = $book->initWithIdOrName($keyword);
-        if ($books) {
-            foreach ($books as $bookInfo) {
-                // Your code to display book information for each book in $books
-                $bookId = $bookInfo->bookID;
-                $bookName = $bookInfo->bookName;
-                $bookPrice = $bookInfo->bookPrice;
-                $publishDate = $bookInfo->publishDate;
-                $bookPic = $bookInfo->bookPic;
-
-                echo "<div class='book'>";
-                echo "<img src='uploads/$bookPic' alt='$bookPic'>";
-                echo "<div class='book-info'>";
-                echo "<h2>" . $bookName . "</h2>";
-                echo "<p>Price: " . $bookPrice . "</p>";
-                echo "<p>Publish Date: " . $publishDate . "</p>";
-                echo "<button>Add to Cart</button>";
-
-                echo "</div>";
-                echo "</div>";
-
-                echo "</div>";
-            }
-        } else {
-            echo "No books found.";
-        }
-    } 
-    include "footer.html";
 
     ?>
 </body>
